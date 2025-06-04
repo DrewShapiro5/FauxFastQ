ea8f2d81b59b5be5652992d05e4010d8467644a9
DrewShapiro5 <drew.shapir05@gmail.com>
2025-06-03 19:19:14 -0600

    Renamed editor file

---
+++ FauxFQ.py
@@ -0,0 +1,8 @@
+import fq_editor
+
+parser = argparse.ArgumentParser(
+                    prog='FauxFastQ',
+                    description='Creates a fastQ file with simulated crispr edits at each point in the reference sequence.',
+                    epilog='https://github.com/DrewShapiro5/FauxFastQ')
+
+parser.
