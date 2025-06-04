39ea25339797f5bd16b3ecfbc2257407171fea6b
DrewShapiro5 <drew.shapir05@gmail.com>
2025-06-04 00:29:35 -0600

    Optimized code for length and readability.

--- fq_editor.py
+++ fq_editor.py
@@ -1,92 +1,146 @@
-import quality_score_approximation
-import sys
-import argparse
+import random
 
-header_str = "@M05774:236:000000000-LKC5Y:1:1107:27241:{index} {dir}:N:0:GATCAGAT+TCCGCGAA"
+class Fqeditor:
+
+    def __init__(self, seed: int, unidirectional: bool = False):
+        self.header_str = "@M05774:236:000000000-LKC5Y:1:1107:27241:{index} {dir}:N:0:GATCAGAT+TCCGCGAA"
+        self._random = random.Random()
+        self._random.seed(seed)
+        self.unidirectional = unidirectional
+
+    # Creates fastq files with deletions for each base pair
+    def create_simulated_fastqs_deletion(self, sequence, quality_forward, quality_reverse, edit_len, file_forward, file_reverse):
+        self._process_edits(sequence,
+                            quality_forward,
+                            quality_reverse,
+                            file_forward,
+                            file_reverse,
+                            edit_func=lambda seq, idx, l, r: seq[:idx] + seq[idx + l:],
+                            iter_len=len(sequence) - edit_len,
+                            edit_len=edit_len)
+
+        """
+        with open('single_deletion_forward.fastq', "w") as f1:
+            with open('single_deletion_reverse.fastq', "w") as f2:
+                for index in range(len(sequence) - edit_len):
+                    header_forward = self.header_str.format(index=index, dir=1)
+                    header_reverse = self.header_str.format(index=index, dir=2)
+                    edited_sequence = sequence[:index] + sequence[index + edit_len:]
+                    #Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
+                    sequence_forward = edited_sequence[:len(quality_forward)]
+                    sequence_reverse = edited_sequence[-len(quality_reverse):][::-1]
+
+                    #Write the contents to the appropriate files
+                    f1.write(f'{header_forward}\n{sequence_forward}\n+\n{quality_forward}\n')
+                    f2.write(f'{header_reverse}\n{dna_complement(sequence_reverse)}\n+\n{quality_reverse}\n')
+            """
+
+    # Creates fastq files with replacements for each base pair
+    def create_simulated_fastqs_replacement(self, sequence, quality_forward, quality_reverse, edit_len, file_forward, file_reverse):
+        self._process_edits(sequence,
+                            quality_forward,
+                            quality_reverse,
+                            file_forward,
+                            file_reverse,
+                            edit_func=single_pair_swap,
+                            iter_len=len(sequence) - edit_len,
+                            edit_len=edit_len)
+
+        """
+        with open('single_swap_forward.fastq', "w") as f1:
+            with open('single_swap_reverse.fastq', "w") as f2:
+                for index in range(len(sequence) - 1):
+                    header_forward = self.header_str.format(index=index, dir=1)
+                    header_reverse = self.header_str.format(index=index, dir=2)
+                    edited_sequence = single_pair_swap(sequence, index)
+                    #Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
+                    sequence_forward = edited_sequence[:len(quality_forward)]
+                    sequence_reverse = edited_sequence[-len(quality_reverse):][::-1]
+
+                    #Write the contents to the appropriate files
+                    f1.write(f'{header_forward}\n{sequence_forward}\n+\n{quality_forward}\n')
+                    f2.write(f'{header_reverse}\n{dna_complement(sequence_reverse)}\n+\n{quality_reverse}\n')
+                    """
+
+    # Creates fastq files with insertions between each pair of base pairs
+    def create_simulated_fastqs_insertion(self, sequence, quality_forward, quality_reverse, edit_len, file_forward, file_reverse):
+        self._process_edits(sequence,
+                            quality_forward,
+                            quality_reverse,
+                            file_forward,
+                            file_reverse,
+                            edit_func=single_pair_insertion,
+                            iter_len=len(sequence) - edit_len,
+                            edit_len=edit_len)
+
+        """
+        with open('single_insertion_forward.fastq', "w") as f1:
+            with open('single_insertion_reverse.fastq', "w") as f2:
+                for index in range(len(sequence) - 1):
+                    header_forward = self.header_str.format(index=index, dir=1)
+                    header_reverse = self.header_str.format(index=index, dir=2)
+                    edited_sequence = single_pair_insertion(sequence, index)
+                    #Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
+                    sequence_forward = edited_sequence[:len(quality_forward)]
+                    sequence_reverse = edited_sequence[-len(quality_reverse):][::-1]
+
+                    #Write the contents to the appropriate files
+                    f1.write(f'{header_forward}\n{sequence_forward}\n+\n{quality_forward}\n')
+                    f2.write(f'{header_reverse}\n{dna_complement(sequence_reverse)}\n+\n{quality_reverse}\n')
+                    """
 
 
+    def _process_edits(self, sequence, quality_forward, quality_reverse,
+                       file_forward, file_reverse,
+                       edit_func, iter_len, edit_len=1):
+        """
+        Driver method for creating edited FASTQ files.
+        Args:
+            edit_func: Function that performs the sequence edit (deletion, swap, or insertion)
+            output_prefix: Prefix for output filenames
+            edit_len: Length of edit (default=1)
+        """
+        # Determine output files based on unidirectional setting
+        output_files = []
+        if self.unidirectional:
+            output_files.append(open(file_forward, 'w'))
+        else:
+            output_files.append(open(file_forward, 'w'))
+            output_files.append(open(file_reverse, 'w'))
 
-def main():
-    # Read in the file containing reference sequence
-    reference_file = "sperm.referenceseq.fa"
-    refence_sequence = open_file_read(reference_file)
-    #Get rid of the first line so that only raw sequence data remains
-    refence_sequence = refence_sequence.splitlines()[1]
-    print(f'reference sequence:\n{refence_sequence}\n')
-    
-    #Store the file path of this original reads
-    reads_file_path_1 = 'reference_reads_compressed/read1.fastq.gz'
-    reads_file_path_2 = 'reference_reads_compressed/read2.fastq.gz'
-    
-    #Get the average quality scores for each file (sampled)
-    qual_string_1 = quality_score_approximation.get_average_quality(reads_file_path_1)
-    qual_string_2 = quality_score_approximation.get_average_quality(reads_file_path_2)
-    print(f'Average quality for forward read:\n{qual_string_1}')
-    print(f'Average quality for backward read:\n{qual_string_2}')
-        
-    #Create the output files
-    create_simulated_fastqs(refence_sequence, qual_string_1, qual_string_2)
-    
-# Creates fastq files with deletions for each base pair
-def create_simulated_fastqs_deletion(sequence, quality_forward, quality_backward):
-    #Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
-    with open('single_deletion_forward.fastq', "w") as f1:
-        with open('single_deletion_reverse.fastq', "w") as f2:
-            for index in range(len(sequence) - DELETION_COUNT):
-                header_forward = header_str.format(index=index, dir=1)
-                header_backward = header_str.format(index=index, dir=2)
-                edited_sequence = sequence[:index] + sequence[index + DELETION_COUNT:]
-                #Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
-                forward_sequence = edited_sequence[:len(quality_forward)]
-                backward_sequence = edited_sequence[-len(quality_backward):][::-1]
-                
-                #Write the contents to the appropriate files
-                f1.write(f'{header_forward}\n{forward_sequence}\n+\n{quality_forward}\n')
-                f2.write(f'{header_backward}\n{dna_complement(backward_sequence)}\n+\n{quality_backward}\n')
-                
-# Creates fastq files with replacements for each base pair
-def create_simulated_fastqs_replacement(sequence, quality_forward, quality_backward):
-    #Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
-    with open('single_swap_forward.fastq', "w") as f1:
-        with open('single_swap_reverse.fastq', "w") as f2:
-            for index in range(len(sequence) - 1):
-                header_forward = header_str.format(index=index, dir=1)
-                header_backward = header_str.format(index=index, dir=2)
-                edited_sequence = single_pair_swap(sequence, index)
-                #Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
-                forward_sequence = edited_sequence[:len(quality_forward)]
-                backward_sequence = edited_sequence[-len(quality_backward):][::-1]
-                
-                #Write the contents to the appropriate files
-                f1.write(f'{header_forward}\n{forward_sequence}\n+\n{quality_forward}\n')
-                f2.write(f'{header_backward}\n{dna_complement(backward_sequence)}\n+\n{quality_backward}\n')
+        for index in range(iter_len):
+            # Generate headers
+            header_forward = self.header_str.format(index=index, dir=1)
+            header_reverse = self.header_str.format(index=index, dir=2)
 
-# Creates fastq files with insertions between each pair of base pairs
-def create_simulated_fastqs_insertion(sequence, quality_forward, quality_backward):
-    #Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
-    with open('single_insertion_forward.fastq', "w") as f1:
-        with open('single_insertion_reverse.fastq', "w") as f2:
-            for index in range(len(sequence) - 1):
-                header_forward = header_str.format(index=index, dir=1)
-                header_backward = header_str.format(index=index, dir=2)
-                edited_sequence = single_pair_insertion(sequence, index)
-                #Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
-                forward_sequence = edited_sequence[:len(quality_forward)]
-                backward_sequence = edited_sequence[-len(quality_backward):][::-1]
-                
-                #Write the contents to the appropriate files
-                f1.write(f'{header_forward}\n{forward_sequence}\n+\n{quality_forward}\n')
-                f2.write(f'{header_backward}\n{dna_complement(backward_sequence)}\n+\n{quality_backward}\n')
+            # Apply the edit
+            edited_sequence = edit_func(sequence, index, edit_len, self._random)
 
-# Replaces
-def single_pair_swap(sequence, index):
+            # Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
+            sequence_forward = edited_sequence[:len(quality_forward)]
+            sequence_reverse = edited_sequence[-len(quality_reverse):][::-1]
+
+            # Write entries
+            write_fastq_entry(output_files[0], header_forward,
+                                    sequence_forward, quality_forward)
+
+            if not self.unidirectional:
+                write_fastq_entry(output_files[1], header_reverse,
+                                        dna_complement(sequence_reverse),
+                                        quality_reverse)
+        # Ensure files are closed
+        for f in output_files:
+            f.close()
+
+# Replaces the base pair at index 'index' with a random base pair
+def single_pair_swap(sequence, index, r: random.Random):
     complement = dna_complement(sequence[index])
     list_sequence = list(sequence)
     list_sequence[index] = complement
     return ''.join(list_sequence)
 
 # Inserts the complement of the pair at index 'index' after the pair
-def single_pair_insertion(sequence, index):
+def single_pair_insertion(sequence, index, r: random.Random):
     complement = dna_complement(sequence[index])
     list_sequence = list(sequence)
     list_sequence.insert(index + 1, complement)
@@ -104,20 +158,15 @@ def dna_complement(sequence):
         'c': 'g',
         'g': 'c'
     }
-    
     complement = []
     for base in sequence:
         complement.append(complement_dict[base])
-    
     return ''.join(complement)
 
-def open_file_read(path):
-    try:
-        with open(path, 'r') as file:
-            return file.read()
-    except FileNotFoundError:
-        print(f"Error: The file '{path}' was not found.")
-        exit(1)
+def random_base(r: random.Random):
+    base_arr = ['A', 'C', 'G', 'T']
+    return base_arr[r.randint(0, len(base_arr) - 1)]
 
-if __name__ == "__main__":
-    main()
+def write_fastq_entry(file, header, sequence, quality):
+    """Helper method to write a single FASTQ entry."""
+    file.write(f'{header}\n{sequence}\n+\n{quality}\n')
