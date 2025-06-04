d87ecbdcfedb0ff5ed38d79ba88c0a03f76fc5f4
DrewShapiro5 <drew.shapir05@gmail.com>
2025-06-04 13:59:27 -0600

    Completed parameter parsing and added code for running the edits

--- FauxFQ.py
+++ FauxFQ.py
@@ -1,9 +1,6 @@
 import pathlib
-import fq_editor
 import quality_score_approximation
 import argparse
-import pyfastx
-
 from fq_editor import Fqeditor
 
 parser = argparse.ArgumentParser(
@@ -12,13 +9,13 @@ parser = argparse.ArgumentParser(
                     epilog='https://github.com/DrewShapiro5/FauxFastQ')
 
 parser.add_argument('reference_fa', type=pathlib.Path,)
+parser.add_argument('output_forward', type=pathlib.Path, help="Path to simulated output file (forward sequence)")
+parser.add_argument('output_reverse', type=pathlib.Path, help="Path to simulated output file (reverse sequence)")
 parser.add_argument('-n', '--edit_length', type=int, default=1, help='Amount of base pairs in the edits.')
 parser.add_argument('--unidirectional', action='store_true', help="Use unidirectional sequencing")
 parser.add_argument('--reference_forward', type=pathlib.Path, help="Path to forward reference fastq file (for quality score sampling)")
 parser.add_argument('--reference_reverse', type=pathlib.Path, help="Path to reverse reference fastq file (for quality score sampling)")
-parser.add_argument('--output_forward', type=pathlib.Path, help="Path to simulated output file (forward sequence)")
-parser.add_argument('--output_reverse', type=pathlib.Path, help="Path to simulated output file (reverse sequence)")
-parser.add_argument('-s', '--seed', type=int, help="Seed for random number generator")
+parser.add_argument('-s', '--seed', type=int, help="Seed for random base generator")
 parser.add_argument('-d', '--deletion', action='store_true', help="Deletion mode")
 parser.add_argument('-i', '--insertion', action='store_true', help="Insertion mode")
 parser.add_argument('-r', '--replacement', action='store_true', help="Replacement mode")
@@ -38,31 +35,42 @@ def main():
     seed = args.seed if args.seed else seq_hash
     unidirectional = args.unidirectional if args.unidirectional else False
 
+    # Get the average quality scores for each reference file (sampled)
     qual_string_forward = ''
     if args.reference_forward:
-        # Store the file path of this original reads
         reference_forward = args.reference_forward
-        qual_string_forward = quality_score_approximation.get_average_quality(reference_forward)
-        print(f'Average quality for forward read:\n{qual_string_forward}')
-
+        qual_string_forward = quality_score_approximation.get_average_quality(str(reference_forward))
+        print(f'Average quality for forward read:\n{qual_string_forward}\n')
     qual_string_reverse = ''
     if args.reference_reverse:
         reference_reverse = args.reference_reverse
-        # Get the average quality scores for each file (sampled)
-        qual_string_reverse = quality_score_approximation.get_average_quality(reference_reverse)
-        print(f'Average quality for reverse read:\n{qual_string_reverse}')
+        qual_string_reverse = quality_score_approximation.get_average_quality(str(reference_reverse))
+        print(f'Average quality for reverse read:\n{qual_string_reverse}\n')
+
+    if args.output_forward == args.output_reverse and not unidirectional:
+        print(f'Output file paths are the same: {args.output_forward}\nNo outputs will be created.')
+        exit(1)
 
     file_mode = 'a' if args.append else 'w'
+    # If in append mode, delete the previous contents of the output files
+    if args.append:
+        with open(args.output_forward, 'w') as _:
+            pass
+        if not args.unidirectional:
+            with open(args.output_reverse, 'w') as _:
+                pass
     fqe = Fqeditor(seed=seed, unidirectional=unidirectional, file_mode=file_mode)
     total_edit_modes = 0
+
     if args.deletion:
         # TODO: for runs without quality score, specify read length
         fqe.create_simulated_fastqs_deletion(sequence=reference_sequence,
                                              quality_forward=qual_string_forward,
                                              quality_reverse=qual_string_reverse,
                                              edit_len=args.edit_length,
-                                             file_forward=args.output_forward,
-                                             file_reverse=args.output_reverse)
+                                             # TODO: make default output file names nice (based on the reference file name?)
+                                             file_forward=args.output_forward if args.output_forward else None,
+                                             file_reverse=args.output_reverse if args.output_reverse else None,)
         total_edit_modes += 1
     if args.insertion:
         # TODO: for runs without quality score, specify read length
@@ -70,8 +78,8 @@ def main():
                                               quality_forward=qual_string_forward,
                                               quality_reverse=qual_string_reverse,
                                               edit_len=args.edit_length,
-                                              file_forward=args.output_forward,
-                                              file_reverse=args.output_reverse)
+                                              file_forward=args.output_forward if args.output_forward else None,
+                                              file_reverse=args.output_reverse if args.output_reverse else None,)
         total_edit_modes += 1
     if args.replacement:
         # TODO: for runs without quality score, specify read length
@@ -79,8 +87,8 @@ def main():
                                                 quality_forward=qual_string_forward,
                                                 quality_reverse=qual_string_reverse,
                                                 edit_len=args.edit_length,
-                                                file_forward=args.output_forward,
-                                                file_reverse=args.output_reverse)
+                                                file_forward=args.output_forward if args.output_forward else None,
+                                                file_reverse=args.output_reverse if args.output_reverse else None,)
         total_edit_modes += 1
     if total_edit_modes > 1 and not args.append:
         print('Warning: multiple edit modes specified without appending. Use --append or only one edit mode.')
