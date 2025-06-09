e18bea06b9cd1ae92b0b0a21712c5b8aeeae8577
DrewShapiro5 <drew.shapir05@gmail.com>
2025-06-09 13:57:06 -0600

    Fixed editing functions so that they work with multiple pairs. Added random functionality.

--- fq_editor.py
+++ fq_editor.py
@@ -1,24 +1,26 @@
 import random
 
-class Fqeditor:
+class FQEditor:
 
-    def __init__(self, seed: int, file_forward, file_reverse, unidirectional: bool = False, file_mode: str = 'w'):
-        self.header_str = "@M05774:236:000000000-LKC5Y:1:1107:27241:{index} {dir}:N:0:GATCAGAT+TCCGCGAA"
+    def __init__(self, seed: int, file_forward, file_reverse, unidirectional: bool = False, file_mode: str = 'w', preserve_case: bool = False, use_random_bases: bool = False):
+        self._header_str = "@M05774:236:000000000-LKC5Y:1:1107:27241:{index} {dir}:N:0:GATCAGAT+TCCGCGAA"
         self._random = random.Random()
         self._random.seed(seed)
-        self.unidirectional = unidirectional
-        self.file_mode = file_mode
-        self.total_reads = 0
+        self._unidirectional = unidirectional
+        self._file_mode = file_mode
+        self._total_reads = 0
         self.file_forward = file_forward
         self.file_reverse = file_reverse
+        self._preserve_case = preserve_case
+        self._use_random_bases = use_random_bases
 
     # Creates fastq files with deletions for each base pair
     def create_simulated_fastqs_deletion(self, sequence, quality_forward, quality_reverse, edit_len):
         self._process_edits(sequence,
                             quality_forward,
                             quality_reverse,
-                            edit_func=lambda seq, idx, l, r: seq[:idx] + seq[idx + l:],
-                            iter_len=len(sequence) - edit_len,
+                            edit_func=lambda seq, idx, l: seq[:idx] + seq[idx + l:],
+                            iter_len=len(sequence) - edit_len + 1,
                             edit_len=edit_len)
 
     # Creates fastq files with replacements for each base pair
@@ -26,8 +28,8 @@ class Fqeditor:
         self._process_edits(sequence,
                             quality_forward,
                             quality_reverse,
-                            edit_func=single_pair_swap,
-                            iter_len=len(sequence) - edit_len,
+                            edit_func=self.pair_swap,
+                            iter_len=len(sequence) - edit_len + 1,
                             edit_len=edit_len)
 
     # Creates fastq files with insertions between each pair of base pairs
@@ -35,8 +37,8 @@ class Fqeditor:
         self._process_edits(sequence,
                             quality_forward,
                             quality_reverse,
-                            edit_func=single_pair_insertion,
-                            iter_len=len(sequence) - 1,
+                            edit_func=self.pair_insertion,
+                            iter_len=len(sequence) + 1,
                             edit_len=edit_len)
 
     def _process_edits(self, sequence, quality_forward, quality_reverse,
@@ -45,24 +47,25 @@ class Fqeditor:
         Driver method for creating edited FASTQ files.
         Args:
             edit_func: Function that performs the sequence edit (deletion, swap, or insertion)
-            output_prefix: Prefix for output filenames
             edit_len: Length of edit (default=1)
         """
+        if not self._preserve_case:
+            sequence = sequence.lower()
         # Determine output files based on unidirectional setting
         output_files = []
-        if self.unidirectional:
-            output_files.append(open(self.file_forward, self.file_mode))
+        if self._unidirectional:
+            output_files.append(open(self.file_forward, self._file_mode))
         else:
-            output_files.append(open(self.file_forward, self.file_mode))
-            output_files.append(open(self.file_reverse, self.file_mode))
+            output_files.append(open(self.file_forward, self._file_mode))
+            output_files.append(open(self.file_reverse, self._file_mode))
 
         for index in range(iter_len):
             # Generate headers
-            header_forward = self.header_str.format(index=index+self.total_reads, dir=1)
-            header_reverse = self.header_str.format(index=index+self.total_reads, dir=2)
+            header_forward = self._header_str.format(index=index + self._total_reads, dir=1)
+            header_reverse = self._header_str.format(index=index + self._total_reads, dir=2)
 
             # Apply the edit
-            edited_sequence = edit_func(sequence, index, edit_len, self._random)
+            edited_sequence = edit_func(sequence, index, edit_len)
 
             # Create forward and backward sequences with slicing. Backward sequence is reversed with slicing.
             sequence_forward = edited_sequence[:len(quality_forward)]
@@ -72,28 +75,46 @@ class Fqeditor:
             write_fastq_entry(output_files[0], header_forward,
                                     sequence_forward, quality_forward)
 
-            if not self.unidirectional:
+            if not self._unidirectional:
                 write_fastq_entry(output_files[1], header_reverse,
                                         dna_complement(sequence_reverse),
                                         quality_reverse)
         # Ensure files are closed
         for f in output_files:
+            print("{} edits written to file {}".format(iter_len, f.name))
             f.close()
-        self.total_reads += iter_len
+        self._total_reads += iter_len
 
-# Replaces the base pair at index 'index' with a random base pair
-def single_pair_swap(sequence, index, edit_len, r: random.Random):
-    complement = dna_complement(sequence[index])
-    list_sequence = list(sequence)
-    list_sequence[index] = complement
-    return ''.join(list_sequence)
+    # Replaces the base pair at index 'index' with a random base pair
+    def pair_swap(self, sequence, index, edit_len):
+        replacement = []
+        list_sequence = list(sequence)
+        if self._use_random_bases:
+            for i in range(edit_len):
+                replacement.append(random_base(self._random))
+        else:
+            replacement = dna_complement(circular_slice(list_sequence, index, edit_len))
+        list_sequence = list_sequence[:index] + [replacement.upper()] + list_sequence[index + edit_len:]
+        return ''.join(list_sequence)
 
-# Inserts the complement of the pair at index 'index' after the pair
-def single_pair_insertion(sequence, index, edit_len, r: random.Random):
-    complement = dna_complement(sequence[index])
-    list_sequence = list(sequence)
-    list_sequence.insert(index + 1, complement)
-    return ''.join(list_sequence)
+    # Inserts the complement of the pair at index 'index' after the pair
+    def pair_insertion(self, sequence, index, edit_len):
+        insertion = ''
+        list_sequence = list(sequence)
+        if self._use_random_bases:
+            for i in range(edit_len):
+                insertion += random_base(self._random)
+        else:
+            insertion = dna_complement(circular_slice(list_sequence, index, edit_len))
+        list_sequence.insert(index, ''.join(insertion).upper())
+        return ''.join(list_sequence)
+
+def circular_slice(arr, start, length):
+    result = []
+    for i in range(length):
+        index = (start + i) % len(arr)
+        result.append(arr[index])
+    return result
 
 # Returns the complement of a given sequence
 def dna_complement(sequence):
