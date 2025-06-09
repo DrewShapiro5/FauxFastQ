95c21a52c8559f09d53f7e1e6daf908facb4c145
DrewShapiro5 <drew.shapir05@gmail.com>
2025-06-09 13:57:47 -0600

    Finished parameter parsing. Improved quality score creation.

--- FauxFQ.py
+++ FauxFQ.py
@@ -1,7 +1,7 @@
 import pathlib
 import quality_score_approximation
 import argparse
-from fq_editor import Fqeditor
+from fq_editor import FQEditor
 
 parser = argparse.ArgumentParser(
                     prog='FauxFastQ',
@@ -13,8 +13,8 @@ parser.add_argument('output_forward', type=pathlib.Path, help="Path to simulated
 parser.add_argument('output_reverse', type=pathlib.Path, help="Path to simulated output file (reverse sequence)")
 parser.add_argument('-n', '--edit_length', type=int, default=1, help='Amount of base pairs in the edits.')
 parser.add_argument('--unidirectional', action='store_true', help="Use unidirectional sequencing")
-parser.add_argument('--reference_forward', type=pathlib.Path, help="Path to forward reference fastq file (for quality score sampling)")
-parser.add_argument('--reference_reverse', type=pathlib.Path, help="Path to reverse reference fastq file (for quality score sampling)")
+parser.add_argument('--reference_forward', type=pathlib.Path, default=None, help="Path to forward reference fastq file (for quality score sampling)")
+parser.add_argument('--reference_reverse', type=pathlib.Path, default=None, help="Path to reverse reference fastq file (for quality score sampling)")
 parser.add_argument('-s', '--seed', type=int, help="Seed for random base generator")
 parser.add_argument('-d', '--deletion', action='store_true', help="Deletion mode")
 parser.add_argument('-i', '--insertion', action='store_true', help="Insertion mode")
@@ -22,10 +22,9 @@ parser.add_argument('-r', '--replacement', action='store_true', help="Replacemen
 parser.add_argument('--append', action='store_true', help="For use with multiple editing modes. Creates a fasta/q file with edits of each specified mode.")
 parser.add_argument('--sample_size', type=int, default=500, help="Sample size for quality sampling from reference files")
 parser.add_argument('--use_quality', type=bool, default=True, help="Output files will contain quality scores by default (fastq format). If set to false, output files will be in fasta format.")
-parser.add_argument('-l', '--read_length', type=int, required=False, help="Use quality score approximation")
+parser.add_argument('-l', '--read_length', type=int, required=False, help="Manually sets the length of reads (overrides length of reference file reads)")
 
 args = parser.parse_args()
-print(args)
 
 def main():
     reference_file = args.reference_fa
@@ -45,30 +44,28 @@ def main():
         print(f'Output file paths are the same: {args.output_forward}\nNo outputs will be created.')
         exit(1)
 
-    # Get the average quality scores for each reference file (sampled)
-    qual_string_forward = None
-    qual_string_reverse = None
-    if args.reference_forward:
-        reference_forward = args.reference_forward
-        qual_string_forward = quality_score_approximation.get_average_quality(str(reference_forward), args.sample_size)
-        print(f'Average quality for forward read:\n{qual_string_forward}\n')
-    elif args.read_length:
-        qual_string_forward = max_quality_string(args.read_length)
-    else:
-        print('No forward reference file provided for quality sampling.'
-              'Provide a reference fastq file with --reference_forward <file>, or specify read length with -l <length>')
-        exit(1)
+    read_length = -1
+    if args.read_length:
+        if args.read_length < 1:
+            print(f'Invalid read length: {args.read_length}. Must be greater than 0.')
+            exit(1)
+        if args.read_length > len(reference_sequence):
+            print(f'Warning: Specified read length {args.read_length} is longer than the length of reference sequence (length={len(reference_sequence)} bp). The length of the reference sequence will be used instead.')
+            read_length = len(reference_sequence)
+        else:
+            read_length = args.read_length
 
-    if args.reference_reverse and not unidirectional:
-        reference_reverse = args.reference_reverse
-        qual_string_reverse = quality_score_approximation.get_average_quality(str(reference_reverse), args.sample_size)
-        print(f'Average quality for reverse read:\n{qual_string_reverse}\n')
-    elif args.read_length and not unidirectional:
-        qual_string_reverse = max_quality_string(args.read_length)
-    elif not unidirectional:
-        print('No reverse reference file provided for quality sampling.'
-              'Provide a reference fastq file with --reference_reverse <file>, or specify read length with -l <length>')
-        exit(1)
+    # Get the average quality scores for forward and reverse reads
+    qual_string_forward = get_quality_score(args.reference_forward,
+                                            args.sample_size,
+                                            'forward',
+                                            read_length,
+                                            False)
+    qual_string_reverse = get_quality_score(args.reference_reverse,
+                                            args.sample_size,
+                                            'reverse',
+                                            read_length,
+                                            not unidirectional)
 
     # If not in append mode, delete the previous contents of the output files
     if not args.append:
@@ -85,9 +82,16 @@ def main():
         print('No edit mode specified. Exiting.')
         exit(0)
 
+    print(f'FauxFastQ arguments:\n'
+          f'forward read length: {len(qual_string_forward)}\n'
+          f'reverse read length: {len(qual_string_reverse)}\n'
+          f'edit length: {args.edit_length}\n'
+          f'append: {args.append}\n'
+          f'unidirectional: {args.unidirectional}\n')
+
     # Create the fastq editor
-    fqe = Fqeditor(seed=seed, unidirectional=unidirectional, file_mode=file_mode, file_forward=str(args.output_forward), file_reverse=str(args.output_reverse))
-    # TODO: runs for fasta, figure out how to index headers correctly with append mode (use sequence hash + edit parameters as the machine id?)
+    fqe = FQEditor(seed=seed, unidirectional=unidirectional, file_mode=file_mode, file_forward=str(args.output_forward), file_reverse=str(args.output_reverse))
+    # runs for fasta, figure out how to index headers correctly with append mode (use sequence hash + edit parameters as the machine id?)
     if args.deletion:
         print('Performing deletions...\n')
         fqe.create_simulated_fastqs_deletion(sequence=reference_sequence,
@@ -117,6 +121,27 @@ def open_file_read(path):
 
 def max_quality_string(length):
     return '!' * length
+
+def get_quality_score(reference, sample_size, direction: str, read_length, throw_error):
+    qual_string = ''
+    if reference is not None:
+        # If reference file exists, sample from it
+        qual_string = quality_score_approximation.get_average_quality(str(reference), sample_size)
+        print(f'Average quality for {direction} read:\n{qual_string}\n')
+        if read_length > -1:
+            if read_length <= len(qual_string):
+                qual_string = qual_string[:read_length]
+            else:
+                qual_string += max_quality_string(args.read_length - len(qual_string))
+                print(f'Warning: Specified read length {read_length} is larger than the length of reference reads in {reference} (length={len(qual_string)} bp).'
+                      f'Missing quality scores will be substituted with \'!\'.\n')
+    elif read_length > -1:
+        qual_string = max_quality_string(read_length)
+    elif throw_error:
+        print(f'No {direction} reference file provided for quality sampling.'
+              f'Provide a reference fastq file with --reference_{direction} <file>, or specify read length with -l <length>')
+        exit(1)
+    return qual_string
 
 if __name__ == "__main__":
     main()
