42783d6e0ef9e5912a070f0575f7338f10eebfad
DrewShapiro5 <drew.shapir05@gmail.com>
2025-06-11 14:35:36 -0600

    Changed example file

--- examples/example.sh
+++ examples/example.sh
@@ -1,1 +1,1 @@
-python3 ~/Desktop/Lab/FauxFastQ/FauxFQ.py sperm.referenceseq.fa o_f.fq o_r.fq -n 1 --reference_forward reference_reads/read1.fastq.gz --reference_reverse reference_reads/read2.fastq.gz -d
+python3 ../FauxFQ.py sperm.referenceseq.fa o_f.fq o_r.fq -n 2 --reference_forward reference_reads/read1.fastq.gz --reference_reverse reference_reads/read2.fastq.gz -r
