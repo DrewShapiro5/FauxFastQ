259e208a578c4fae7add192f59b16fe240ee821f
DrewShapiro5 <drew.shapir05@gmail.com>
2026-03-03 16:30:10 -0700

    Updated examples

--- examples/example2.sh
+++ examples/example2.sh
@@ -1,3 +1,1 @@
 python3 ../FauxFQ.py sperm.referenceseq.fa o5_f.fq o5_r.fq -n 5 --reference_forward reference_reads/read1.fastq.gz --reference_reverse reference_reads/read2.fastq.gz -d -i -r
-xattr -d com.apple.quarantine o_f.fq
-xattr -d com.apple.quarantine o_r.fq
